4598019294d75e5613a46c70b91c0fa882a9093a
MrOganes <112868498+MrOganes@users.noreply.github.com>
2022-11-24 18:19:26 +0300

    Update ProjectJS.js

--- ProjectJS.js
+++ ProjectJS.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
     $('#faq_id').click(function() {
         a=a+1;
         if(a%2!=0){
-            $("#text, #faq_id").css( "color", "rgb(241, 77,52)");
+            $("#faq_id").css( "color", "rgb(241, 77,52)");
             $("#one").css("border", "3px solid rgb(241, 77,52)");
         }
         else{
-            $("#text, #faq_id").css( "color", "black");
+            $("#faq_id").css( "color", "black");
             $("#one").css("border", "0px");
         }
     });
