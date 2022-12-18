02e0317dc4484f85b811e02f14c7f0446c817aa3
MrSergey98 <mrsergey98@mail.ru>
2022-12-18 19:28:30 +0300

    q

--- ProjectJS.js
+++ ProjectJS.js
@@ -11,26 +11,6 @@ var window;
 var ResizeSensor;
 var jQuery;
 
-function animate({timing, draw, duration}) {
-
-    let start = performance.now();
-  
-    requestAnimationFrame(function animate(time) {
-      // timeFraction изменяется от 0 до 1
-      let timeFraction = (time - start) / duration;
-      if (timeFraction > 1) timeFraction = 1;
-  
-      // вычисление текущего состояния анимации
-      let progress = timing(timeFraction);
-  
-      draw(progress); // отрисовать её
-  
-      if (timeFraction < 1) {
-        requestAnimationFrame(animate);
-      }
-  
-    });
-}
 
 function Admin_menu_on() {
     $("#Admin-menu").show(300);
